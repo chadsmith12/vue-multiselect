1f0fd8596a67298c50cc5e03ab5838be302790a9
Chad Smith <smith.chad12@gmail.com>
2019-07-28 20:20:55 -0500

    Initial Commit of component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,61 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-sm">
+          <label for="addValue">Add Item</label>
+          <input type="text" class="form-control" id="addValue" v-model="newItem"/>
+          <button type="button" class="btn btn-primary" @click="addValue">Add Value</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <Multiselect 
+            label="Please Select Your Fruits..."
+            placeholder="Search Fruits..."
+            :items="items"
+            v-model="selectedItems"/>
+        </div>
+        <div class="col-sm">
+          Current Selected Items:
+          <ul>
+            <li v-for="item in selectedItems" :key="item">
+              {{ item }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import Multiselect from '@/components/Multiselect.vue';
+import { SelectItem } from '@/models/SelectItem';
 
 @Component({
   components: {
     HelloWorld,
+    Multiselect,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public items: string[] = ['Apple', 'Banana', 'Orange', 'Lemon', 'Lime', 'Mango', 'Pear', 'Peach'];
+  public selectedItems: string[] = ['Apple', 'Orange'];
+  public preselectedItems: string[] = [];
+  public newItem: string = '';
+
+  public addValue(): void {
+    if (this.newItem.length > 0) {
+      this.items.push(this.newItem);
+      this.newItem = '';
+    }
+  }
+}
 </script>
 
 <style lang="scss">
---
+++ src/components/Multiselect.vue
@@ -0,0 +1,179 @@
+<template>
+<div>
+    <label>{{ label }}</label>
+
+    <div class="search-box">
+        <input type="search"
+            class="form-control"
+            :placeholder="placeholder" 
+            :aria-placeholder="placeholder"
+            v-model="searchValue">
+    </div>
+    <div class="selected" :class="{open: hasSelectedItems}">
+        <div v-for="item in selectedItems" :key="item" class="choice">
+            {{ item }}<i class="fa fa-times unselect" @click="clickedItem(item)"></i>
+        </div>
+    </div>
+    <div class="list-group list-group-flush">
+        <a v-for="(item, index) in filteredItems" 
+            :key="index" 
+            href="#" 
+            @click="clickedItem(item)" 
+            class="list-group-item list-group-item-action"
+            :class="{active: isSelected(item)}">
+
+            <input type="checkbox" hidden />
+            <i class="far fa-square"></i>
+            <span>{{ item }}</span>
+        </a>
+    </div>
+</div>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
+import { SelectItem } from '@/models/SelectItem';
+
+@Component
+export default class Multiselect extends Vue {
+    public selectedItems: string[] = [];
+    public searchValue: string = '';
+    @Prop() private label!: string;
+    @Prop({default: 'Search...'}) private placeholder!: string;
+    @Prop() private items!: string[];
+    @Prop() private value!: string[];
+
+    private get hasSelectedItems(): boolean {
+        return this.selectedItems.length > 0;
+    }
+
+    private get filteredItems(): string[] {
+        if (!this.isSearching) {
+            return this.items;
+        }
+
+        const searchLower = this.searchValue.toLowerCase();
+        return this.items.filter((item) => item.toLowerCase().indexOf(searchLower) > -1);
+    }
+
+    private get isSearching(): boolean {
+        return this.searchValue.length >  0;
+    }
+
+    private clickedItem(selectedItem: string): void {
+        const isItemSelected = this.isSelected(selectedItem);
+        if (isItemSelected) {
+            const indexToDelete = this.selectedItems.indexOf(selectedItem);
+            this.$delete(this.selectedItems, indexToDelete);
+        } else {
+            this.selectedItems.push(selectedItem);
+        }
+
+        this.onInputChange();
+    }
+
+    @Emit('input')
+    private onInputChange() {
+        return this.selectedItems;
+    }
+
+    private isSelected(selectedItem: string): boolean {
+        return this.selectedItems.includes(selectedItem);
+    }
+
+    private mounted(): void {
+        this.selectedItems = this.value.map((item) => item);
+    }
+}
+</script>
+
+<style scoped>
+.search-box {
+  position: relative;
+}
+.search-box .form-control {
+  background-color: transparent;
+  border-radius: 0.25rem 0.25rem 0 0;
+  margin-bottom: -1px;
+  padding-left: 2.75rem;
+  position: relative;
+  z-index: 10;
+}
+.search-box:before {
+  content: "\f002";
+  color: #999;
+  font-family: "Font Awesome 5 Free";
+  font-weight: 900;
+  padding: 0.5rem 1rem;
+  position: absolute;
+}
+
+.selected {
+  border-right: 1px solid #ced4da;
+  border-left: 1px solid #ced4da;
+  display: flex;
+  max-height: 0;
+  overflow-x: auto;
+  overflow-y: hidden;
+  transition: 0.15s all ease-in-out;
+}
+.selected.open {
+  max-height: 3rem;
+}
+.selected .choice {
+  background: #f5f6f8;
+  border-right: 1px solid #ced4da;
+  display: flex;
+  align-items: center;
+  font-size: 0.75rem;
+  height: 2rem;
+  padding-left: 0.5rem;
+}
+.selected .choice i {
+  color: #999;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-size: 0.7rem;
+  height: 100%;
+  padding: 0 0.75rem;
+  transition: 0.15s color ease-in-out;
+}
+.selected .choice i:hover {
+  color: #000;
+}
+
+.list-group {
+  border: 1px solid #ced4da;
+  border-radius: 0 0 0.25rem 0.25rem;
+  height: 205px;
+  overflow-y: auto;
+}
+.list-group .list-group-item {
+  border-top: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  margin: 0;
+  outline: none;
+  padding: 0.5rem 1rem;
+  transition: 0.15s background-color ease-in-out;
+}
+.list-group .list-group-item i {
+  margin-right: 1rem;
+  transform: rotate(270deg);
+  transition: 0.15s transform ease-in-out;
+}
+.list-group .list-group-item i:before {
+  transition: 0.15s transform ease-in-out;
+}
+.list-group .list-group-item.active i {
+  transform: rotate(360deg);
+}
+.list-group .list-group-item.active i:before {
+  content: "\f00c";
+  font-weight: 900;
+}
+
+</style>
